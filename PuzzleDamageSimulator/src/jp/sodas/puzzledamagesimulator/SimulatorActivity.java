33213a2876a4316e9ebc5d8f931689a0f0b4ee34
hey-ss <snow6334hey@gmail.com>
2014-12-16 21:37:35 +0900

    First commit

---
+++ PuzzleDamageSimulator/src/jp/sodas/puzzledamagesimulator/SimulatorActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 2014/12/08
+ * Copyright (C) 2014 Koga Laboratory. All rights reserved.
+ *
+ */
+package jp.sodas.puzzledamagesimulator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SimulatorActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.simulator);
+  }
+}
