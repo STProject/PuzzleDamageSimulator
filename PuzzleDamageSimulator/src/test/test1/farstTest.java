dd5ee3b4c577853791ac5a2537bd26172854ca3b
togo <togo@mk.ces.kyutech.ac.jp>
2015-01-05 19:12:56 +0900

    alltests implement

---
+++ PuzzleDamageSimulator/src/test/test1/farstTest.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 2015/01/05
+ * Copyright (C) 2015 Koga Laboratory. All rights reserved.
+ *
+ */
+package test.test1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class farstTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
